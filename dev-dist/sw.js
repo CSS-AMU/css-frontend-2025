c5ef6077573bda694d1dcd9e9665ef0d3b2bee4c
Sameer khan <121482820+Sameerkhan9412@users.noreply.github.com>
2025-01-26 21:55:18 +0530

    Slider added

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0jao43j121"
+    "revision": "0.9f94gpccl18"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
